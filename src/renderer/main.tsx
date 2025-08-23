690afb6f513cd9f348fd447c8d050d276962d45c
DidiG03 <sefridkapllani@gmail.com>
2025-08-23 03:38:53 +0200

    feat: register service worker

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -4,7 +4,9 @@ import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { routes } from './routes';
 import './styles/index.css';
 import { offlineQueue } from './utils/offlineQueue';
+import { registerSW } from 'virtual:pwa-register';
 
+registerSW();
 const router = createHashRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
